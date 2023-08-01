2915ab2caa2ae569cb1545efc34272088dc37971
radsylph <andresalvarezsilva6@gmail.com>
2023-08-01 09:16:57 -0400

    feat:Added NgOptimizedImage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  Component, // importar el decorador Component (sirve para crear componentes)
+  ElementRef, // importar el decorador ElementRef (sirve para manipular el DOM al igual que ViewChild pero este es mas complejo de usar)
+  InjectorType, // importar el decorador InjectorType (sirve para inyectar dependencias en el constructor de la clase)
+  ViewChild, // importar el decorador ViewChild (sirve para manipular el DOM al igual que ElementRef pero este es mas facil de usar)
+  inject, // importar el decorador inject (sirve para inyectar dependencias )
+} from '@angular/core';
+
+import { BtnComponent } from './components/btn/btn.component'; // importar el componente BtnComponent
+import { InputComponent } from './components/input/input.component'; // importar el componente InputComponent
+import { ProductComponent } from './components/product/product.component'; // importar el componente ProductComponent
+import { Product } from './models/product.model'; // importar la interface Product
+import { NgOptimizedImage } from '@angular/common';
+
+import { HttpClient } from '@angular/common/http'; // importar el modulo HttpClient (sirve para hacer peticiones http)
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // decorador Component (sirve para crear componentes)
+  selector: 'app-root', // selector del componente
+  templateUrl: './app.component.html', // template del componente
+  styleUrls: ['./app.component.css'], // estilos del componente
 })
 export class AppComponent {
-  title = 'Time_managment_tool';
+  // clase AppComponent (aqui esta todo el codigo del componente))
+  @ViewChild(InputComponent, { static: true })
+  InputComponent: // ViewChild del componente InputComponent (es como un getElementById)
+  InputComponent | undefined;
+  @ViewChild(BtnComponent, { static: true })
+  BtnComponent: // ViewChild del componente BtnComponent (es como un getElementById)
+  BtnComponent | undefined;
+
+  http = inject(HttpClient); // inyectar dependencia HttpClient en la variable http (es como un singleton)
+  products: Product[] = []; // declarar la variable products como un array de Product (es como un useState de react)
+
+  btnHandler() {
+    // funcion btnHandler (se ejecuta cuando se hace click en el boton)
+    console.log('test');
+    this.InputComponent?.changeName();
+  }
+
+  ngOnInit() {
+    // funcion ngOninit (se ejecuta cuando se inicia el componente, como un useEffect de react)
+    console.log('hola init');
+    this.http.get<Product[]>('https://fakestoreapi.com/products').subscribe({
+      // peticion http get a la api fakestoreapi.com/products
+      next: (data) => {
+        console.log(data);
+        this.products = data;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './components/input/input.component';
+import { BtnComponent } from './components/btn/btn.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, InputComponent, BtnComponent, ProductComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    HttpClientModule,
+    NgOptimizedImage,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
